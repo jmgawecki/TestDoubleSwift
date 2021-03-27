2ec8f489e0befdb6ecdc9936bba152d1108d608f
Jakub Gawecki <71930261+jmgawecki@users.noreply.github.com>
2021-03-27 15:26:46 +0000

    Initial Commit

---
+++ TestDoubleSwiftTests/TestDoubleSwiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  TestDoubleSwiftTests.swift
+//  TestDoubleSwiftTests
+//
+//  Created by Jakub Gawecki on 27/03/2021.
+//
+
+import XCTest
+@testable import TestDoubleSwift
+
+class TestDoubleSwiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
